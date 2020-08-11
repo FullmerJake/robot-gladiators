62bc4133d6228f6a1852330d03d3d0d2e7701e11
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-08-11 16:02:05 -0600

    playing around with functions and variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("what is your robot's name?")
+// what is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a mesasge");
+//what is this?
+console.log("Our Robot's name is " + playerName);
+
+// this will do math and log 20
+console.log(10 + 10);
+
+var name = "your name";
+console.log(name);
+
+
+
+
+//This creates a function named "fight"
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+// fight();
